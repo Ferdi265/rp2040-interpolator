9d96d87ede9e76c167e0b300ecf255af3b45a3a0
Ferdinand Bachmann <theferdi265@gmail.com>
2022-04-28 22:49:11 +0200

    python: add python implementation

---
+++ python/rp2040_interp/interp.py
@@ -0,0 +1,206 @@
+from typing import List
+from dataclasses import dataclass
+
+ubits = lambda n, b: n & ((1 << b) - 1)
+sbits = lambda n, b: (-1 << (b - 1)) | ubits(n, b) if n & (1 << (b - 1)) else ubits(n, b)
+u8 = lambda n: ubits(n, 8)
+u16 = lambda n: ubits(n, 16)
+u32 = lambda n: ubits(n, 32)
+s32 = lambda n: sbits(n, 32)
+
+@dataclass
+class InterpCtrl:
+    shift: int
+    mask_lsb: int
+    mask_msb: int
+    is_signed: bool
+    cross_input: bool
+    cross_result: bool
+    add_raw: bool
+    force_msb: int
+    blend: bool
+    clamp: bool
+    overf0: bool
+    overf1: bool
+    overf: bool
+    _reserved0: int
+
+    @staticmethod
+    def from_reg(value: int) -> 'InterpCtrl':
+        return InterpCtrl(
+            shift        =      (value >>  0) & 0b11111,
+            mask_lsb     =      (value >>  5) & 0b11111,
+            mask_msb     =      (value >> 10) & 0b11111,
+            is_signed    = bool((value >> 15) & 1),
+            cross_input  = bool((value >> 16) & 1),
+            cross_result = bool((value >> 17) & 1),
+            add_raw      = bool((value >> 18) & 1),
+            force_msb    =      (value >> 19) & 0b11,
+            blend        = bool((value >> 21) & 1),
+            clamp        = bool((value >> 22) & 1),
+            overf0       = bool((value >> 23) & 1),
+            overf1       = bool((value >> 24) & 1),
+            overf        = bool((value >> 25) & 1),
+            _reserved0   =      (value >> 26) & 0b111111,
+        )
+
+    def to_reg(self) -> int:
+        return (
+            ((self.shift        & 0b11111)  <<  0) |
+            ((self.mask_lsb     & 0b11111)  <<  5) |
+            ((self.mask_msb     & 0b11111)  << 10) |
+            ((self.is_signed    & 1)        << 15) |
+            ((self.cross_input  & 1)        << 16) |
+            ((self.cross_result & 1)        << 17) |
+            ((self.add_raw      & 1)        << 18) |
+            ((self.force_msb    & 0b11)     << 19) |
+            ((self.blend        & 1)        << 21) |
+            ((self.clamp        & 1)        << 22) |
+            ((self.overf0       & 1)        << 23) |
+            ((self.overf1       & 1)        << 24) |
+            ((self.overf        & 1)        << 25) |
+            ((self._reserved0   & 0b111111) << 26)
+        )
+
+@dataclass
+class Interp:
+    n: int
+    accum: List[int] # len = 2
+    base: List[int]  # len = 3
+    ctrl: List[int]  # len = 2
+
+    _smresult: List[int] # len = 2
+    _result: List[int]   # len = 3
+
+    def __init__(self, n: int = 0):
+        assert n in [0, 1], "invalid interpolator index"
+        self.n = n
+        self.accum = [0, 0]
+        self.base = [0, 0, 0]
+        self.ctrl = [0, 0]
+        self._smresult = [0, 0]
+        self._result = [0, 0, 0]
+        self.update()
+
+    def pop(self, i: int) -> int:
+        self.update()
+        v = self._result[i]
+        self._writeback()
+        return v
+
+    def peek(self, i: int) -> int:
+        self.update()
+        return self._result[i]
+
+    def peekraw(self, i: int) -> int:
+        self.update()
+        return self._smresult[i]
+
+    def add(self, i: int, value: int):
+        self.accum[i] += value
+
+    def base01(self, value: int):
+        self._writebase01(value)
+
+    def update(self):
+        self._adjustbits()
+        ctrl0 = InterpCtrl.from_reg(self.ctrl[0])
+        ctrl1 = InterpCtrl.from_reg(self.ctrl[1])
+
+        do_clamp = (ctrl0.clamp and self.n == 1)
+        do_blend = (ctrl0.blend and self.n == 0)
+
+        ctrl0.clamp = do_clamp
+        ctrl0.blend = do_blend
+        ctrl0._reserved0 = 0
+        ctrl1.clamp = False
+        ctrl1.blend = False
+        ctrl1.overf0 = False
+        ctrl1.overf1 = False
+        ctrl1.overf = False
+        ctrl1._reserved0 = 0
+
+        input0 = self.accum[1 if ctrl0.cross_input else 0]
+        input1 = self.accum[0 if ctrl1.cross_input else 1]
+
+        mask0 = ((1 << (ctrl0.mask_msb + 1)) - 1) & ~((1 << ctrl0.mask_lsb) - 1)
+        mask1 = ((1 << (ctrl1.mask_msb + 1)) - 1) & ~((1 << ctrl1.mask_lsb) - 1)
+
+        uresult0 = (input0 >> ctrl0.shift) & mask0
+        uresult1 = (input1 >> ctrl1.shift) & mask1
+
+        overf0 = bool((input0 >> ctrl0.shift) & ~((1 << (ctrl0.mask_msb + 1)) - 1))
+        overf1 = bool((input1 >> ctrl1.shift) & ~((1 << (ctrl1.mask_msb + 1)) - 1))
+        overf = overf0 or overf1
+
+        sextmask0 = (-1 << ctrl0.mask_msb) & ((1 << 32) - 1) if (uresult0 & (1 << ctrl0.mask_msb)) else 0
+        sextmask1 = (-1 << ctrl1.mask_msb) & ((1 << 32) - 1) if (uresult1 & (1 << ctrl1.mask_msb)) else 0
+
+        sresult0 = uresult0 | sextmask0
+        sresult1 = uresult1 | sextmask1
+
+        result0 = sresult0 if ctrl0.is_signed else uresult0
+        result1 = sresult1 if ctrl1.is_signed else uresult1
+
+        addresult0 = u32(self.base[0] + (input0 if ctrl0.add_raw else result0))
+        addresult1 = u32(self.base[1] + (input1 if ctrl1.add_raw else result1))
+        addresult2 = u32(self.base[2] + result0 + result1)
+
+        uclamp0 = self.base[0] if result0 < self.base[0] else (self.base[1] if result0 > self.base[1] else result0)
+        sclamp0 = self.base[0] if s32(result0) < s32(self.base[0]) else (self.base[1] if s32(result0) > s32(self.base[1]) else result0)
+        clamp0 = u32(sclamp0) if ctrl0.is_signed else uclamp0
+
+        alpha1 = u8(result1)
+        ublend1 = self.base[0] + (alpha1 * (self.base[1] - self.base[0]) >> 8)
+        sblend1 = s32(self.base[0]) + (alpha1 * (s32(self.base[1]) - s32(self.base[0])) >> 8)
+        blend1 = u32(sblend1 if ctrl1.is_signed else ublend1)
+
+        self._smresult[0] = result0
+        self._smresult[1] = result1
+        self._result[0] = alpha1 if do_blend else ((clamp0 if do_clamp else addresult0) | (ctrl0.force_msb << 28))
+        self._result[1] = (blend1 if do_blend else addresult1) | (ctrl1.force_msb << 28)
+        self._result[2] = u32(self.base[2] + result0) if do_blend else addresult2
+
+        ctrl0.overf0 = overf0
+        ctrl0.overf1 = overf1
+        ctrl0.overf = overf
+        self.ctrl[0] = ctrl0.to_reg()
+        self.ctrl[1] = ctrl1.to_reg()
+
+    def _writeback(self):
+        ctrl0 = InterpCtrl.from_reg(self.ctrl[0])
+        ctrl1 = InterpCtrl.from_reg(self.ctrl[1])
+
+        self.accum[0] = self._result[1 if ctrl0.cross_result else 0]
+        self.accum[1] = self._result[0 if ctrl1.cross_result else 1]
+
+        self.update();
+
+    def _writebase01(self, v: int):
+        ctrl0 = InterpCtrl.from_reg(self.ctrl[0])
+        ctrl1 = InterpCtrl.from_reg(self.ctrl[1])
+
+        do_blend = (ctrl0.blend and self.n == 0)
+
+        input0 = u16(v)
+        input1 = u16(v >> 16)
+
+        sextmask0 = u32(-1 << 15) if (input0 & (1 << 15)) else 0
+        sextmask1 = u32(-1 << 15) if (input1 & (1 << 15)) else 0
+
+        base0 = input0 | sextmask0 if (ctrl1.is_signed if do_blend else ctrl0.is_signed) else input0
+        base1 = input1 | sextmask1 if ctrl1.is_signed else input1
+
+        self.base[0] = base0;
+        self.base[1] = base1;
+
+        self.update()
+
+    def _adjustbits(self):
+        self.accum[0] = u32(self.accum[0])
+        self.accum[1] = u32(self.accum[1])
+        self.base[0]  = u32(self.base[0])
+        self.base[1]  = u32(self.base[1])
+        self.base[2]  = u32(self.base[2])
+        self.ctrl[0]  = u32(self.ctrl[0])
+        self.ctrl[1]  = u32(self.ctrl[1])
---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "rp2040_interp",
+    description = "Software Emulation Library for the RP2040 Interpolator peripheral",
+    author = "Ferdinand Bachmann",
+    author_email = "theferdi265@gmail.com",
+    packages = ["rp2040_interp"],
+    entry_points = {},
+    python_requires = ">=3.5",
+    install_requires = []
+)
